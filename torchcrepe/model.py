6260f308bb63c52d70908f842a37d7e6b71ff8c0
Max Morrison <maxrmorrison@gmail.com>
2020-06-03 22:55:45 +0000

    Switched to tiny model
    Former-commit-id: 22aa3dd41635c55a252dc032d24a40a903307b96

--- torchcrepe/model.py
+++ torchcrepe/model.py
@@ -25,19 +25,19 @@ class Crepe(nn.Module):
         global __weights_dict
         __weights_dict = load_weights(weight_file)
 
-        self.conv1 = self.__conv(2, name='conv1', in_channels=1, out_channels=1024, kernel_size=(512, 1), stride=(4, 1), groups=1, bias=True)
-        self.conv1_BN = self.__batch_normalization(2, 'conv1-BN', num_features=1024, eps=0.0010000000474974513, momentum=0.0)
-        self.conv2 = self.__conv(2, name='conv2', in_channels=1024, out_channels=128, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
-        self.conv2_BN = self.__batch_normalization(2, 'conv2-BN', num_features=128, eps=0.0010000000474974513, momentum=0.0)
-        self.conv3 = self.__conv(2, name='conv3', in_channels=128, out_channels=128, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
-        self.conv3_BN = self.__batch_normalization(2, 'conv3-BN', num_features=128, eps=0.0010000000474974513, momentum=0.0)
-        self.conv4 = self.__conv(2, name='conv4', in_channels=128, out_channels=128, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
-        self.conv4_BN = self.__batch_normalization(2, 'conv4-BN', num_features=128, eps=0.0010000000474974513, momentum=0.0)
-        self.conv5 = self.__conv(2, name='conv5', in_channels=128, out_channels=256, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
-        self.conv5_BN = self.__batch_normalization(2, 'conv5-BN', num_features=256, eps=0.0010000000474974513, momentum=0.0)
-        self.conv6 = self.__conv(2, name='conv6', in_channels=256, out_channels=512, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
-        self.conv6_BN = self.__batch_normalization(2, 'conv6-BN', num_features=512, eps=0.0010000000474974513, momentum=0.0)
-        self.classifier = self.__dense(name = 'classifier', in_features = 2048, out_features = 360, bias = True)
+        self.conv1 = self.__conv(2, name='conv1', in_channels=1, out_channels=128, kernel_size=(512, 1), stride=(4, 1), groups=1, bias=True)
+        self.conv1_BN = self.__batch_normalization(2, 'conv1-BN', num_features=128, eps=0.0010000000474974513, momentum=0.0)
+        self.conv2 = self.__conv(2, name='conv2', in_channels=128, out_channels=16, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
+        self.conv2_BN = self.__batch_normalization(2, 'conv2-BN', num_features=16, eps=0.0010000000474974513, momentum=0.0)
+        self.conv3 = self.__conv(2, name='conv3', in_channels=16, out_channels=16, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
+        self.conv3_BN = self.__batch_normalization(2, 'conv3-BN', num_features=16, eps=0.0010000000474974513, momentum=0.0)
+        self.conv4 = self.__conv(2, name='conv4', in_channels=16, out_channels=16, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
+        self.conv4_BN = self.__batch_normalization(2, 'conv4-BN', num_features=16, eps=0.0010000000474974513, momentum=0.0)
+        self.conv5 = self.__conv(2, name='conv5', in_channels=16, out_channels=32, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
+        self.conv5_BN = self.__batch_normalization(2, 'conv5-BN', num_features=32, eps=0.0010000000474974513, momentum=0.0)
+        self.conv6 = self.__conv(2, name='conv6', in_channels=32, out_channels=64, kernel_size=(64, 1), stride=(1, 1), groups=1, bias=True)
+        self.conv6_BN = self.__batch_normalization(2, 'conv6-BN', num_features=64, eps=0.0010000000474974513, momentum=0.0)
+        self.classifier = self.__dense(name = 'classifier', in_features = 256, out_features = 360, bias = True)
 
     def forward(self, x):
         input_reshape   = x[:, None, :, None]
@@ -77,11 +77,23 @@ class Crepe(nn.Module):
         conv6_BN        = self.conv6_BN(conv6_activation)
         conv6_maxpool, conv6_maxpool_idx = F.max_pool2d(conv6_BN, kernel_size=(2, 1), stride=(2, 1), padding=0, ceil_mode=False, return_indices=True)
         conv6_dropout   = F.dropout(input = conv6_maxpool, p = 0.25, training = self.training, inplace = True)
-        flatten         = conv6_dropout.reshape(-1, 2048)
+        flatten         = conv6_dropout.reshape(-1, 256)
         classifier      = self.classifier(flatten)
         classifier_activation = F.sigmoid(classifier)
         return classifier_activation
 
+
+    @staticmethod
+    def __conv(dim, name, **kwargs):
+        if   dim == 1:  layer = nn.Conv1d(**kwargs)
+        elif dim == 2:  layer = nn.Conv2d(**kwargs)
+        elif dim == 3:  layer = nn.Conv3d(**kwargs)
+        else:           raise NotImplementedError()
+
+        layer.state_dict()['weight'].copy_(torch.from_numpy(__weights_dict[name]['weights']))
+        if 'bias' in __weights_dict[name]:
+            layer.state_dict()['bias'].copy_(torch.from_numpy(__weights_dict[name]['bias']))
+        return layer
 
     @staticmethod
     def __batch_normalization(dim, name, **kwargs):
@@ -102,18 +114,6 @@ class Crepe(nn.Module):
 
         layer.state_dict()['running_mean'].copy_(torch.from_numpy(__weights_dict[name]['mean']))
         layer.state_dict()['running_var'].copy_(torch.from_numpy(__weights_dict[name]['var']))
-        return layer
-
-    @staticmethod
-    def __conv(dim, name, **kwargs):
-        if   dim == 1:  layer = nn.Conv1d(**kwargs)
-        elif dim == 2:  layer = nn.Conv2d(**kwargs)
-        elif dim == 3:  layer = nn.Conv3d(**kwargs)
-        else:           raise NotImplementedError()
-
-        layer.state_dict()['weight'].copy_(torch.from_numpy(__weights_dict[name]['weights']))
-        if 'bias' in __weights_dict[name]:
-            layer.state_dict()['bias'].copy_(torch.from_numpy(__weights_dict[name]['bias']))
         return layer
 
     @staticmethod
