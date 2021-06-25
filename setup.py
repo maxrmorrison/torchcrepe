08b36ebe8b443ac1d2a9655192e268b3f1b19f34
Max Morrison <maxrmorrison@gmail.com>
2021-06-25 17:03:50 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', encoding='utf8') as file:
 setup(
     name='torchcrepe',
     description='Pytorch implementation of CREPE pitch tracker',
-    version='0.0.14',
+    version='0.0.15',
     author='Max Morrison',
     author_email='maxrmorrison@gmail.com',
     url='https://github.com/maxrmorrison/torchcrepe',
