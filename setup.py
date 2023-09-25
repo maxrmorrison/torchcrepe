edeec05a7a1a27535ea82e0f2837c762d9ebbdf0
Tom Hunn <tom_hunn@me.com>
2023-09-25 13:49:20 +1000

    update librosa version range

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup
 
 
-with open('README.md', encoding='utf8') as file:
+with open("README.md", encoding="utf8") as file:
     long_description = file.read()
 
 
 setup(
-    name='torchcrepe',
-    description='Pytorch implementation of CREPE pitch tracker',
-    version='0.0.22',
-    author='Max Morrison',
-    author_email='maxrmorrison@gmail.com',
-    url='https://github.com/maxrmorrison/torchcrepe',
-    install_requires=['librosa>=0.9.1', 'resampy', 'scipy', 'torch', 'tqdm'],
-    packages=['torchcrepe'],
-    package_data={'torchcrepe': ['assets/*']},
+    name="torchcrepe",
+    description="Pytorch implementation of CREPE pitch tracker",
+    version="0.0.22",
+    author="Max Morrison",
+    author_email="maxrmorrison@gmail.com",
+    url="https://github.com/maxrmorrison/torchcrepe",
+    install_requires=["librosa>=0.9.1", "resampy", "scipy", "torch", "tqdm"],
+    packages=["torchcrepe"],
+    package_data={"torchcrepe": ["assets/*"]},
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords=['pitch', 'audio', 'speech', 'music', 'pytorch', 'crepe'],
-    classifiers=['License :: OSI Approved :: MIT License'],
-    license='MIT')
+    long_description_content_type="text/markdown",
+    keywords=["pitch", "audio", "speech", "music", "pytorch", "crepe"],
+    classifiers=["License :: OSI Approved :: MIT License"],
+    license="MIT",
+)
