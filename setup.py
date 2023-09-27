2bc7d0d154cac0db753f6d8e93567b8fe032edd5
Tom Hunn <tom_hunn@me.com>
2023-09-27 16:28:13 +1000

    add test + support librosa > 0.9.1

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ with open('README.md', encoding='utf8') as file:
 setup(
     name='torchcrepe',
     description='Pytorch implementation of CREPE pitch tracker',
-    version='0.0.21',
+    version='0.0.22',
     author='Max Morrison',
     author_email='maxrmorrison@gmail.com',
     url='https://github.com/maxrmorrison/torchcrepe',
-    install_requires=['librosa==0.9.1', 'resampy', 'scipy', 'torch', 'tqdm'],
+    install_requires=['librosa>=0.9.1', 'resampy', 'scipy', 'torch', 'tqdm'],
     packages=['torchcrepe'],
     package_data={'torchcrepe': ['assets/*']},
     long_description=long_description,
