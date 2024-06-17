e2c305878ec7a89aec85a01f8d223a75a36d30b6
Max Morrison <maxrmorrison@gmail.com>
2024-06-17 10:58:08 -0700

    Torchaudio loading; windows testing

--- torchcrepe/load.py
+++ torchcrepe/load.py
@@ -2,20 +2,15 @@ import os
 
 import numpy as np
 import torch
-import torchcrepe
+import torchaudio
 from scipy.io import wavfile
+
+import torchcrepe
 
 
 def audio(filename):
     """Load audio from disk"""
-    sample_rate, audio = wavfile.read(filename)
-
-    # Convert to float32
-    if audio.dtype == np.int16:
-        audio = audio.astype(np.float32) / np.iinfo(np.int16).max
-
-    # PyTorch is not compatible with non-writeable arrays, so we make a copy
-    return torch.tensor(np.copy(audio))[None], sample_rate
+    return torchaudio.load(filename)
 
 
 def model(device, capacity='full'):
